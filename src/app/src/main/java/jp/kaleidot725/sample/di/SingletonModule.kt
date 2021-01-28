3728b7e7826ce6894c4f7a23d89a447efc7e558d
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2021-01-28 23:19:14 +0900

    fix: rename ApplicationModule to SingletonModule

--- src/app/src/main/java/jp/kaleidot725/sample/di/SingletonModule.kt
+++ src/app/src/main/java/jp/kaleidot725/sample/di/SingletonModule.kt
@@ -5,19 +5,20 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.components.SingletonComponent
 import jp.kaleidot725.sample.model.FooService
 import jp.kaleidot725.sample.model.FooServiceImpl
 import jp.kaleidot725.sample.model.FooUsecase
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 abstract class ApplicationBindsModule {
     @Binds
     abstract fun bindFooService(fooServiceImpl: FooServiceImpl) : FooService
 }
 
 @Module
-@InstallIn(ApplicationComponent::class)
+@InstallIn(SingletonComponent::class)
 object ApplicationProvidesModule {
     @Provides
     fun provideFooUseCase(fooService: FooService) : FooUsecase {
